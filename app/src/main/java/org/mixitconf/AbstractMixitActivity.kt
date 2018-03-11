ee7f6478c7a40919f2390ac0ebd7c66b8f3b51bd
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-03-11 19:00:48 +0100

    Adds about dialog

--- app/src/main/java/org/mixitconf/AbstractMixitActivity.kt
+++ app/src/main/java/org/mixitconf/AbstractMixitActivity.kt
@@ -1,11 +1,49 @@
 package org.mixitconf
 
 import android.content.Intent
+import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import org.mixitconf.fragment.DialogAboutFragment
 
 abstract class AbstractMixitActivity() : AppCompatActivity() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.global, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.menu_about -> {
+                val dial = DialogAboutFragment()
+                dial.show(fragmentManager, resources.getString(R.string.about_title))
+                return true
+            }
+            R.id.menu_map -> {
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     fun getNavigationItemSelectedListener() = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
