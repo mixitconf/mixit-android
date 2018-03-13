6f96eab92f91728238b309c158d0a1e04b5ab3fa
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-03-13 19:54:17 +0100

    Adds home page header

--- app/src/main/java/org/mixitconf/MainActivity.kt
+++ app/src/main/java/org/mixitconf/MainActivity.kt
@@ -1,7 +1,12 @@
 package org.mixitconf
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import org.mixitconf.repository.TalkReader
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.mixitconf.service.hasIntentPackage
 
 open class MainActivity : AbstractMixitActivity() {
 
@@ -9,10 +14,32 @@ open class MainActivity : AbstractMixitActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        TalkReader.getInstance(baseContext)
+        twitterLink.setOnClickListener(OnLinkClickListener(this, Social.Twitter))
+        emailLink.setOnClickListener(OnLinkClickListener(this, Social.Email))
     }
 
-    override fun onResume() {
-        super.onResume()
+    class OnLinkClickListener(val context: Context, val media: Social) : View.OnClickListener {
+
+        override fun onClick(view: View?) {
+            when(media){
+                Social.Twitter -> sendTwitter()
+                Social.Email -> sendEmail()
+            }
+
+        }
+
+        private fun sendTwitter() {
+            val hasTwitterApp = context.hasIntentPackage("com.twitter.android")
+            val intentUri = if (hasTwitterApp) "twitter://user?screen_name=mixitconf" else "https://twitter.com/mixitconf"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(intentUri)).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+        }
+
+        private fun sendEmail() {
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:contact@mix-it.fr"))
+            context.startActivity(intent)
+        }
     }
+
+    enum class Social { Twitter, Email }
 }
--- app/src/main/java/org/mixitconf/service/Utils.kt
+++ app/src/main/java/org/mixitconf/service/Utils.kt
@@ -1,6 +1,8 @@
 package org.mixitconf.service
 
 import android.annotation.TargetApi
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.text.Html
 import com.github.rjeschke.txtmark.Processor
@@ -16,6 +18,7 @@ class Utils {
 val SPECIAL_SLUG_CHARACTERS = mapOf<Char, Char>(Pair('é','e'), Pair('è','e'),Pair('ï','i'), Pair(' ','_'), Pair('ê','e'), Pair('à','a'), Pair('-','_'))
 
 // String extension to convert markdown to HTML
+// ============================================
 fun String.markdownToHtml() = if (isNullOrEmpty()) null else Processor.process(this).toHtml()
 
 @TargetApi(Build.VERSION_CODES.KITKAT_WATCH)
@@ -24,6 +27,7 @@ fun String.toHtml() = if (isNullOrEmpty()) null else Html.fromHtml(this)
 fun String.toSlug(): String = toLowerCase().toCharArray().map { if (SPECIAL_SLUG_CHARACTERS.get(it) == null) it else SPECIAL_SLUG_CHARACTERS.get(it) }.joinToString("")
 
 // Date extensions
+// ============================================
 fun Date.adjust(day: Int, hour: Int, minute: Int): Date {
     val calendar = Calendar.getInstance(Locale.FRANCE)
     calendar.set(2018, 3, day, hour, minute, 0)
@@ -44,3 +48,14 @@ fun Date.addMinutes(amount: Int): Date {
     return calendar.time
 }
 
+// Context extension
+// ============================================
+fun Context.hasIntentPackage(type: String): Boolean {
+    try{
+        packageManager.getPackageInfo(type, 0)
+        return true
+    }
+    catch (e:PackageManager.NameNotFoundException){
+        return false
+    }
+}
