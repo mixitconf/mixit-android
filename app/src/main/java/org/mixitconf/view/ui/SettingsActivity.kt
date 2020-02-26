0a3d2bba18f0525a23e0887cc765009f10fce962
Guillaume EHRET <guillaume@dev-mind.fr>
2020-02-26 19:26:27 +0100

    Add new parameter in settings to add user email

--- app/src/main/java/org/mixitconf/view/ui/SettingsActivity.kt
+++ app/src/main/java/org/mixitconf/view/ui/SettingsActivity.kt
@@ -19,7 +19,7 @@ class SettingsActivity : MixitActivity() {
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            setPreferencesFromResource(R.xml.mixit_preferences, rootKey)
         }
 
         override fun onPause() {
